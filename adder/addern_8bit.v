16ef0b0c4af3ea7bc6f7d7773740b109c8b02936
sss22213 <n0404.n0404@gmail.com>
2016-03-20 22:44:19 +0800

    ADD addern_8bit.v

--- adder/addern_8bit.v
+++ adder/addern_8bit.v
@@ -1,19 +1,22 @@
-module addern(carryin,X,Y,S,carryout);
+module addern(carryin,X,Y,S,sub,carryout);
 
+input [7:0]sub;
 input carryin;
 input [7:0]X,Y;
 output reg [7:0]S;
 output reg carryout;
 reg [8:0]C;
 integer K;
+reg [7:0]temp;
 
 always @(X,Y,carryin)
 begin
-	C[0]=carryin;
+	C[0]=sub[0];
 	for(K=0;K<8;K=K+1)
 	begin
-		S[K]=X[K]^Y[K]^C[K];
-		C[K+1]=(X[K]&Y[K])|(X[K]&C[K])|(Y[K]&C[K]);
+		temp[K]=Y[K]^sub[K];
+		S[K]=X[K]^temp[K]^C[K];
+		C[K+1]=(X[K]&temp[K])|(X[K]&C[K])|(temp[K]&C[K]);
 	end
 	carryout=C[7];
 end
@@ -22,16 +25,20 @@ endmodule
 
 module test;
 
+reg [7:0]sub;
 reg carryin;
-reg [7:0]X,Y;
+reg [7:0]X;
+reg [7:0]Y;
 wire [7:0]S;
 wire carryout;
-addern ADD1(carryin,X,Y,S,carryout);
+addern ADD1(carryin,X,Y,S,sub,carryout);
+
 initial 
 begin
-	X=8'b00000001;
-	Y=8'b00000011;
+	X=8'b10001011;
+	Y=8'b00001001;
+	sub=8'b11111111;
 	carryin=0;
-	$monitor("%x",S);
+	$monitor("X=%x Y=%x S=%x",X,Y,S);
 end
 endmodule
