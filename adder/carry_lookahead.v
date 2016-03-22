54ccf1b5a766a6d5c02bf60fc03c6c9bf4ff32f7
sss22213 <n0404.n0404@gmail.com>
2016-03-22 23:06:25 +0800

    ADD carry_adder

--- adder/carry_lookahead.v
+++ adder/carry_lookahead.v
@@ -1,18 +1,21 @@
 //main module
-module carry_lookahead(X,Y,Cin,S,Cout);
+module carry_lookahead(X,Y,Sub,Cin,S,Cout);
 input [7:0]X;
 input [7:0]Y;
 input [7:0]Cin;
-output [7:0]S;
+input [7:0]Sub;
+output wire[7:0]S;
 output [7:0]Cout;
 
 wire [7:0]g;
 wire [7:0]q;
+wire  [7:0]temp;
 
-assign g=X&Y;
-assign p=x^Y;
+assign temp=Y^Sub;
+assign g=X&temp;
+assign p=X^temp;
 
-assign Cout[0]=Cin;
+assign Cout[0]=0;
 assign Cout[1]=g[0]|(p[0]&Cout[0]);
 assign Cout[2]=g[1]|(p[1]&g[0])|(p[1]&p[0]&Cout[0]);
 assign Cout[3]=g[2]|(p[2]&g[1])|(p[2]&p[1]&g[0])|(p[2]&p[1]&p[0]&Cout[0]);
@@ -20,28 +23,28 @@ assign Cout[4]=g[3]|(p[3]&g[2])|(p[3]&p[2]&g[1])|(p[3]&p[2]&p[1]&g[0])|(p[3]&p[2
 assign Cout[5]=g[4]|(p[4]&g[3])|(p[4]&p[3]&g[2])|(p[4]&p[3]&p[2]&g[1])|(p[4]&p[3]&p[2]&p[1]&g[0])|(p[4]&p[3]&p[2]&p[1]&p[0]&Cout[0]);
 assign Cout[6]=g[5]|(p[5]&g[4])|(p[5]&p[4]&g[3])|(p[5]&p[4]&p[3]&g[2])|(p[5]&p[4]&p[3]&p[2]&g[1])|(p[5]&p[4]&p[3]&p[2]&p[1]&g[0])|(p[5]&p[4]&p[3]&p[2]&p[1]&p[0]&Cout[0]);
 assign Cout[7]=g[6]|(p[6]&g[5])|(p[6]&p[5]&g[4])|(p[6]&p[5]&p[4]&g[3])|(p[6]&p[5]&p[4]&p[3]&g[2])|(p[6]&p[5]&p[4]&p[3]&p[2]&g[1])|(p[6]&p[5]&p[4]&p[3]&p[2]&p[1]&g[0])|(p[6]&p[5]&p[4]&p[3]&p[2]&p[1]&p[0]&Cout[0]);
+
+assign S=X^temp^Cout;
 endmodule
 
 //test module
 module test;
 
-carry_lookahead DHU(X,Y,Cin,S,Cout);
-
 reg [7:0]X;
 reg [7:0]Y;
 reg [7:0]Cin;
-reg [7:0]S;
-reg [7:0]Cout;
-
-wire [7:0]g;
-wire [7:0]q;
+reg [7:0]Sub;
+wire [7:0]S;
+wire [7:0]Cout;
+carry_lookahead DHU(X,Y,Sub,Cin,S,Cout);
 
 initial 
 begin
-	X=0;
-	Y=0;
-	Cin=0;
-	S=0;
+	Cin=8'b00000000;
+	X=8'b00000010;
+	Y=8'b00000101;
+	Sub=8'b00000000;
+	$monitor("%d %d",Cout,S);
 end
 
 
